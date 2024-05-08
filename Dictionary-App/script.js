a4967e5f3c395e920fd4658c1db81588c48549c8
shalimar-mehra <shalimarmehraa@gmail.com>
2024-05-08 17:17:34 +0530

    ok

---
+++ Dictionary-App/script.js
@@ -0,0 +1,28 @@
+// searchinput
+// searchbtn
+
+const dictionary = (word) => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "2c4a07aca7msh07d3cecc1b0148bp18936fjsn2d9c92c95796",
+      "X-RapidAPI-Host": "dictionary-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+  fetch(
+    'https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary?word=' + word,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+        
+      wordheading.innerHTML = response.word;
+      definition.innerHTML = response.definition;
+    })
+    .catch(err => console.error(err));
+};
+
+searchbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  dictionary(searchinput)
+});
